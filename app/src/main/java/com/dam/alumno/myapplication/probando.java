5c8381b48d238ecf46958ad08758c7b59dfe9ce6
Markinho85 <marcogd85@gmail.com>
2016-01-26 13:07:41 +0100

    Esto es una prueba

--- app/src/main/java/com/dam/alumno/myapplication/probando.java
+++ app/src/main/java/com/dam/alumno/myapplication/probando.java
@@ -4,19 +4,6 @@ package com.dam.alumno.myapplication;
  * Created by alumno on 26/01/2016.
  */
 public class probando {
-    ljljlkjkl lkjljlk ljlk;
 
-    /**
-     * Created by alumno on 26/01/2016.
-     */
-    public static class peobleesgay {
-        String peoblesgay;
-    }
 
-    /**
-     * Created by alumno on 26/01/2016.
-     */
-    public static class prueba {
-    String a;
-    }
 }
